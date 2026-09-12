a6d8673ee211cb082357e0837cc1cd0d1fb024bc
nicopowell <nicopowell30@gmail.com>
2026-09-12 14:17:58 -0300

    finished problem 791A

---
+++ 800-899/791A-BearAndBigBrother/main.cpp
@@ -0,0 +1,19 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    short int a, b, years=0;
+
+    cin >> a >> b;
+
+    while(a <= b) {
+        a *= 3;
+        b *= 2;
+        years++;
+    }
+
+    cout << years << '\n';
+
+    return 0;
+}
