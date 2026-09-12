e81b5b13443ffcee79238a5d6cfba27b62306bb6
nicopowell <nicopowell30@gmail.com>
2026-09-12 12:17:49 -0300

    finished problem 281A

---
+++ 800-899/281A-WordCapitalization/main.cpp
@@ -0,0 +1,16 @@
+#include <iostream>
+#include <cctype>
+
+using namespace std;
+
+int main() {
+    string word;
+
+    cin >> word;
+
+    word[0] = toupper(word[0]);
+
+    cout << word << '\n';
+
+    return 0;
+}
