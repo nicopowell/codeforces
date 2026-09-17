7b1c32e0a4f642d9364bb5bf7778eb69022dffbc
nicopowell <nicopowell30@gmail.com>
2026-09-17 13:28:35 -0300

    finished problem 977A

---
+++ 800-899/977A-WrongSubstraction/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long n;
+    int k;
+
+    cin >> n >> k;
+
+    while (k != 0) {
+        if (n % 10 != 0) n--;
+        else n /= 10;
+        k--;
+    }
+
+    cout << n << '\n';
+
+    return 0;
+}
