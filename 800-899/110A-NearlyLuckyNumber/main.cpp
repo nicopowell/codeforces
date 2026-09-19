4bbaf4552ebf35bf0c44536ea030ac8fb7ba9ab5
nicopowell <nicopowell30@gmail.com>
2026-09-18 22:16:38 -0300

    finished problem 110A

---
+++ 800-899/110A-NearlyLuckyNumber/main.cpp
@@ -0,0 +1,20 @@
+#include <iostream>
+
+using namespace std;
+
+int main() {
+    long long int n;
+    int count=0;
+
+    cin >> n;
+
+    while(n!=0) {
+        if (n%10 == 4 || n%10 == 7) count++;
+        n /= 10;
+    }
+
+    if (count == 4 || count == 7) cout << "YES" << '\n';
+    else cout << "NO" << '\n';
+
+    return 0;
+}
